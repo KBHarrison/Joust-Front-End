8d313297512eb4887bd26947f940be64c155235f
Kyle Harrison <kharrison@lingotek.com>
2020-12-01 20:13:32 -0700

    Actually made reactive

--- src/components/Character.js
+++ src/components/Character.js
@@ -9,10 +9,11 @@ const Character = (props) => {
     let class_name = "knight " + props.id +  " " + props.position[props.id].direction
     let left = 5*props.position[props.id].x - 1 + '%'
     let top = (5*props.position[props.id].y) + '%'
+    let height = "50%"
     return (
-        <div style={{left, top, position:'absolute'}}>
+        <div style={{left, top, position:'absolute', height}}>
             <span className="label">Player {props.id + 1}</span>
-            <img  title={"Player " + (props.id + 1)} src={Knight} className={class_name}/>
+            <img  title={"Player " + (props.id + 1)} style={{height}} src={Knight} className={class_name}/>
         </div>
     )
 }
